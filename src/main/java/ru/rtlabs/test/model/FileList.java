29bf25a47169aea907533ba3f3037919f3f7ba3c
Sergey Kobylyanskiy <sergey.kobylyansky@rtlabs.ru>
2016-12-07 11:38:09 +0300

    add project

---
+++ src/main/java/ru/rtlabs/test/model/FileList.java
@@ -0,0 +1,31 @@
+package ru.rtlabs.test.model;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileList {
+    private List<String> fileList;
+
+    public FileList() {}
+
+    public FileList(String s) {
+        fileList = new ArrayList();
+        File directory = new File(s);
+        if(directory.exists() & directory.isDirectory()) {
+            for (File e : directory.listFiles()) {
+                if (e.isFile()) {
+                    fileList.add(e.getName());
+                }
+            }
+        }
+    }
+
+    public List<String> getFileList() {
+        return fileList;
+    }
+
+    public void setFileList(List<String> fileList) {
+        this.fileList = fileList;
+    }
+}
---
+++ src/main/java/ru/rtlabs/test/rest/FileBoxRest.java
@@ -0,0 +1,62 @@
+package ru.rtlabs.test.rest;
+
+import ru.rtlabs.test.model.FileList;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FileBoxRest {
+    private Properties prop = new Properties();
+    private static final Logger logger = LogManager.getLogger(FileBoxRest.class);
+
+    public FileBoxRest() {
+        logger.info("Пытаемcя загрузить файл с настройками");
+        try {
+            InputStream in = getClass().getResourceAsStream("/prop.properties");
+            prop.load(in);
+            in.close();
+            logger.info("Файл с настройками загружен");
+            logger.info("Папка для отображения: " + prop.getProperty("DIR"));
+        } catch (IOException | NullPointerException e) {
+            logger.error("Файл с настройки не удалось загрузить", e);
+
+        }
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public FileList getFileList() {
+        logger.info("Вызван метод getFileList");
+        FileList fileList = new FileList(prop.getProperty("DIR"));
+        return fileList;
+
+    }
+
+    @GET
+    @Path("/{filename}")
+    public Response getFileData(@PathParam("filename") String filename) {
+        logger.info("Вызван метод getFileData с параметром " + filename);
+        File file = new File(prop.getProperty("DIR"), filename);
+        if (file.exists() & file.isFile()) {
+            logger.info("Файл найден, передаём его клиенту");
+            return Response.ok()
+                    .type(MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", "attachment; filename=" + filename)
+                    .entity(file)
+                    .build();
+        } else {
+            logger.error("Файл " + filename + " в папке " + prop.getProperty("DIR") + " не найден");
+            return Response.status(404).entity("not found").build();
+        }
+    }
+
+}
